2d7ecc460b4b9b019d8c765a8a078a941c127888
Ella Walsh <78115761+elwalsh22@users.noreply.github.com>
2022-01-26 17:55:45 -0500

    2.5 complete - practice with debugging

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("👍 viewDidLoad has run!")
