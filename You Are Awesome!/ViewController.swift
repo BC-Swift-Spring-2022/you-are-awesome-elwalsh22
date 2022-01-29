a87e6d30edf73efaecdae53fb5cb1ea0ee441452
Ella Walsh <78115761+elwalsh22@users.noreply.github.com>
2022-01-29 13:56:37 -0500

    2.11 commit randomization

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,20 +25,28 @@ class ViewController: UIViewController {
         let messages = ["You Are Awesome!",
                         "You Are Great!",
                         "You Are Fantastic!",
-                        "Fabulous? That's You!"]
+                        "When the Genius Bar Needs Help, They Call You!",
+                        "Fabulous? That's You!",
+                        "You've Got The Design Skills of Jony Ive"]
         
-        messageLabel.text = messages[messageNumber]
-        messageNumber+=1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }
+        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        //messageLabel.text=message[messageNumber
+        //messageNumber +=1
+        //if messageNumber == message.count {
+        //messageNumber =0
+        //}
+        
         //let imageName = "image" + String(imageNumber)
-        let imageName = "image\(imageNumber)" 
+        let imageName = "image\(Int.random(in: 0...totalNumberOfImages))"
         imageView.image = UIImage(named: imageName)
-        imageNumber = imageNumber+1
-        if imageNumber == 10 {
-            imageNumber = 0
-        }
+        
+        //let imageName = "image\(imageNumber)"
+        //imageView.image= UIImage(named: imageName
+        //imageNumber = imageNumber +1
+        //if imageNumber == 10{
+        //imageNumber = 0
+        //}
+        
         
         
         
