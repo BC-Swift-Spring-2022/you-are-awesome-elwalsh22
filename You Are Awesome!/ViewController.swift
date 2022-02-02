d722569519dbd267d2a9a59103e874bee88f7d61
Ella Walsh <78115761+elwalsh22@users.noreply.github.com>
2022-02-01 21:16:00 -0500

    2.15 commit - sound and asset organization

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -6,15 +6,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
-    
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = -1
     var messageNumber = -1
     let totalNumberOfImages = 9
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +35,6 @@ class ViewController: UIViewController {
         repeat {
             newMessageNumber = Int.random(in: 0...messages.count-1)
         } while messageNumber == newMessageNumber
-        
         messageNumber = newMessageNumber
         messageLabel.text = messages[messageNumber]
         
@@ -42,9 +42,21 @@ class ViewController: UIViewController {
         repeat {
             newImageNumber = Int.random(in: 0...totalNumberOfImages)
         } while imageNumber == newImageNumber
-        
         imageNumber = newImageNumber
         imageView.image = UIImage(named: "image\(imageNumber)")
+        
+        if let sound = NSDataAsset(name: "sound0") {
+            do{
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("😡 ERROR: \(error.localizedDescription)Could not initialize AVAudioPlayer object.")
+            }
+            
+            
+        } else {
+            print("😡 ERROR: Could not read data from file sound0")
+        }
 
     
         
